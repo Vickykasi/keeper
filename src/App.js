90f16f3e65d978ee389edb4345b134791c4a6450
Vickykasi <112267266+Vickykasi@users.noreply.github.com>
2023-04-05 12:45:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Keep from './component/keep';
+import "./component/keep.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Keep/>
+    </div>
+  );
+}
+
+export default App;
